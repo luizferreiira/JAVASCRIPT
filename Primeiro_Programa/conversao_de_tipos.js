5be223cd409589dfa28fdfabfe584f45006b4d48
luizferreiira <luizferreiraa34@gmail.com>
2022-01-23 04:15:55 -0300

    Primeiro_Programa/

---
+++ Primeiro_Programa/conversao_de_tipos.js
@@ -0,0 +1,7 @@
+console.log("Conversão de tipos");
+
+console.log("2" + "2");
+console.log("ano" + 2022);
+console.log(parseInt("2") + parseInt("2"));
+
+console.log(parseFloat("10") / parseFloat("2"));
---
+++ Primeiro_Programa/operacoes_aritmeticas.js
@@ -0,0 +1,11 @@
+console.log("Operações Aritiméticas");
+
+console.log(2+2);
+console.log((10+8)*2);
+
+console.log("Ano" + 2022);
+
+//Adição: +
+//Subtração: -
+//Multiplicação: *
+//Divisão: /
