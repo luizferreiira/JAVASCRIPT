6af8d0f56ccb0e2f16b46ddae85297b12d6e4530
luizferreiira <luizferreiraa34@gmail.com>
2022-07-06 03:06:27 -0300

    New Changes

---
+++ W3Schools-JS/7-EVENTS/scripts.js
@@ -0,0 +1,6 @@
+button = document.getElementById('btn').addEventListener('click',date);
+//click, mouseover, mouseout, keydown, load
+
+function date(){
+    document.getElementById('demo').innerHTML = Date(); 
+}
---
+++ W3Schools-JS/8-STRINGS/scripts.js
@@ -0,0 +1,6 @@
+let nome = "Luiz"; //String sempre entre aspas simples ou duplas;
+
+document.getElementById('demo').innerHTML = "Seja bem-vindo ao \"W3Schools\": " + nome; // as barras invertidas servem para identificar se é string ou se estou usando aspas para destacar uma palavra.
+alert(`${nome} seu nome tem ${nome.length} caracteres`);
+
+x = new String("Fernando"); //objeto
