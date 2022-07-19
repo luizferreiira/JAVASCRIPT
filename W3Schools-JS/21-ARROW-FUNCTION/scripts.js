449c078db76f250e689488eac7494d2fd8716d25
luizferreiira <luizferreiraa34@gmail.com>
2022-07-18 22:00:52 -0300

    New changes

---
+++ W3Schools-JS/21-ARROW-FUNCTION/scripts.js
@@ -0,0 +1,14 @@
+
+let myFunction = (a,b) => a * b;
+let hello = () => "Hello, World!";
+let helloUniverse = (val) => "Hello, " + val;
+
+
+
+
+
+document.getElementById('demo').innerHTML = myFunction(10,5);
+document.getElementById('demo2').innerHTML = hello();
+document.getElementById('demo3').innerHTML = helloUniverse('Universe!');
+
+
